a15b021783ce57ec8397065b00d2f429bffd6ce0
Mochamad Syahrial Alzaidan <88835497+syahrialalzaidan@users.noreply.github.com>
2024-01-18 01:06:23 +0700

    feat:mentor landing

--- src/components/mentor/MentorBox.tsx
+++ src/components/mentor/MentorBox.tsx
@@ -1,20 +1,27 @@
 import Image from "next/image";
 import { AiOutlineRight } from "react-icons/ai";
 
-export default function MentorBox() {
+interface MentorBoxProps {
+    name: string;
+    role: string;
+}
+
+export default function MentorBox({ name, role }: MentorBoxProps) {
   return (
-    <div className="bg-white shadow-lg w-[350px] h-[150px] py-4 flex gap-8">
-      <Image
-        src="/mentor/auva.jpeg"
-        width={130}
-        height={130}
-        alt="mentor-box"
-      />
+    <div className="bg-white shadow-lg rounded-lg w-[350px] h-[150px] py-4 px-6 flex gap-8">
+      <div className="w-32 aspect-square bg-slate-300 rounded-full relative overflow-hidden">
+        <Image
+          src={"/mentor/auva.jpeg"}
+          fill={true}
+          alt="profile"
+          className="object-cover w-full h-full"
+        />
+      </div>
 
       <div className="flex flex-col justify-between">
         <div>
-          <h1 className="text-xl font-semibold">Auva</h1>
-          <p className="text-sm text-secondary">UI/UX Designer</p>
+          <h1 className="text-xl font-semibold">{name}</h1>
+          <p className="text-sm text-secondary">{role}</p>
         </div>
         <button className="bg-[#FF6B2D] text-white px-4 py-2 flex gap-2 items-center rounded-full font-normal">
           <p>Details</p>
